3269d05d25ec48be47e6fed03e77df9b522a73ab
Shashank Soni <shashanksoni815@gmail.com>
2025-05-11 02:58:28 +0530

    models

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,1 +1,19 @@
-const asyncHandler = (fn) => () => {}
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             messsage: err.messsage
+
+//         })
+//     }
+// }
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export { asyncHandler }
