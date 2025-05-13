d13f4d65c8770fd32641db2c1f68af8418ebac20
Shashank Soni <shashanksoni815@gmail.com>
2025-05-14 01:34:04 +0530

    loggout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -151,7 +151,7 @@ const loginUser = asyncHandler(async (req, res) => {
     // send cookies
 
     const {email, username, password} = req.body
-    if(!username || !email) {
+    if(!username && !email) {
         throw new ApiError(400, "username or email is required")
     }
 
@@ -192,7 +192,27 @@ const loginUser = asyncHandler(async (req, res) => {
 })
 
 const logoutUser = asyncHandler(async (req, res) => {
-    
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            },
+        },
+            {
+                new: true
+            }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out"))
 })
 
 export { registerUser, loginUser, logoutUser }
