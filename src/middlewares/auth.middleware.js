84778af66869fdf2f2963c711cb7e560d4da961d
Shashank Soni <shashanksoni815@gmail.com>
2025-05-13 23:58:30 +0530

    middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
-    req.cookies?.accessToken || req.header("Authorization")
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
 })
 
