5951e836b3b8dac043e14983d1551616680bf1e2
Shashank Soni <shashanksoni815@gmail.com>
2025-07-01 01:03:17 +0530

    new

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -270,7 +270,9 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 })
 
 const changeCurrentPassword = asyncHandler(async (req, res) => {
-    
+    const {oldPassword, newPassword} = req.body
+
+    const user = await User.findById(req.user?.id)
 })
 
 export { registerUser, loginUser, logoutUser, refreshAccessToken }
